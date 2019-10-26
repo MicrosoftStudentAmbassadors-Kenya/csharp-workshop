02b619eab3b88c9e8a31c9576800bdbade7dbb51
Humphryshikunzi <humphryshikunzi9@gmail.com>
2019-10-26 15:18:21 +0300

    changed passsword to 0000

--- 06_BankaccountProject/Program.cs
+++ 06_BankaccountProject/Program.cs
@@ -9,7 +9,7 @@ namespace Exercise__Git
 
             //Declare  variables
             int verificationCode, pintrial = 1;
-            string pin, userName, userNameLower, realPin = "1234",  savedBankccountNumber = "0100353689800", bankAccountNumber, recipients;
+            string pin, userName, userNameLower, realPin = "0000",  savedBankccountNumber = "0100353689800", bankAccountNumber, recipients;
 
            // label
             rex:   
