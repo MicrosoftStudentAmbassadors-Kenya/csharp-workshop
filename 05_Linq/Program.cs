4ffd4b4d381e587d9f2c881a9501380654a018fe
edwin muraya <edwinkamaumuraya0@outlook.com>
2019-10-28 18:27:05 +0300

    made some code to serialize object

--- 05_Linq/Program.cs
+++ 05_Linq/Program.cs
@@ -1,19 +1,13 @@
 ﻿using System.IO;
 using System;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace _05_Linq
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var football = ReadFile(@"./football.json");
-      Console.WriteLine("Hello World!");
-
-      var obj = JsonConvert.DeserializeObject<Football>(football);
-      System.Console.WriteLine(obj);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
-  }
 }
